103eab19c8acce47567f11bf42c1d2a4d0564c87
AUSDEV-TG <46772726+AUSDEV-TG@users.noreply.github.com>
2020-01-15 15:54:40 +0800

    1.1

---
+++ changelog.md
@@ -0,0 +1,20 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [1.1] - 2020-01-15
+
+### Added
+
+- Function to alter the html links so that the downloaded site works offline.
+
+- Argument to set the index of the offline site.
+
+### Changed
+
+- Saving files with a name that already exists in the directory will append a number to the end of the file name.
--- swcs.py
+++ swcs.py
@@ -68,19 +68,48 @@ def get_urls(parsed_url, page, url_path, ignore):
     return urls
 
 
-def save_html(url, output):
+def save_html(url, output, fileName):
     # Get the html source from the url
     source = urllib.request.urlopen(url).read()
 
-    # Create a safe name for the file
-    fileName = url.split('/')[-2].replace(' ', '_') + '.html'
+    if fileName is None:
+        # Create a safe name for the file
+        fileName = url.split('/')[-2].replace(' ', '_') + '.html'
+
+        # Check for files with the same name
+        sameName = []
+        for root, dirs, files in os.walk(output):
+            if fileName in files:
+                sameName.append(files)
+
+        if len(sameName) > 0:
+            fileName = f"{fileName[:-5]}({len(sameName)}).html"
 
     # Save the source to the output path
     with open("{}\{}".format(output, fileName), 'wb') as file:
         file.write(source)
 
+    return fileName
 
-def main(url, output, ignore, path):
+
+def fix_links(url, directory, fileName):
+    # Open the html file in the specified directory
+    html = open(os.path.join(directory, fileName), encoding='utf8')
+    page = BeautifulSoup(html, 'lxml')
+
+    for element in page.find_all('a', {"href": True}):
+        # Find the links containing the domain
+        if url in element['href']:
+            # Replace the online link with a local link
+            element['href'] = element['href'].split(
+                '/')[-2].replace(' ', '_') + '.html'
+
+    # Save the html file
+    with open("{}\{}".format(directory, fileName), 'wb') as file:
+        file.write(page.prettify("utf-8"))
+
+
+def main(url, output, ignore, path, fix, index):
     saved_files = set()
     to_check = [url]
     max_checks = 10
@@ -89,6 +118,13 @@ def main(url, output, ignore, path):
     start = time.time()
 
     try:
+        if index is not None:
+            # Save the index and fix the links in the file
+            print(f"Saving index.html from {index}")
+            fileName = save_html(index, output, "index.html")
+            print("Fixing Links...")
+            fix_links(url, output, fileName)
+
         while to_check and max_checks:
             link = to_check.pop(0)
             links = process_url(link, path, ignore)
@@ -99,7 +135,10 @@ def main(url, output, ignore, path):
                     saved_files.add(link)
                     to_check.append(link)
                     print(f"Saving Page {len(to_check):>3} : {link}")
-                    save_html(link, output)
+                    fileName = save_html(link, output, None)
+                    if fix:
+                        print("Fixing Links...")
+                        fix_links(url, output, fileName)
 
             max_checks -= 1
     except HTTPError:
@@ -139,6 +178,13 @@ if __name__ == '__main__':
     parser.add_argument("-p", "--path", type=str,
                         help="the path of the url to be crawled")
 
+    # Boolean arg to determine whether the fix_links function should be called
+    parser.add_argument("-f", "--fix", default=False, action="store_true",
+                        help="Flag to fix the links in the downloaded html")
+
+    # Argument to set the index of the site
+    parser.add_argument("--index", type=str, help="sets the index of the site")
+
     args = parser.parse_args()
 
-    main(args.url, args.output, args.ignore, args.path)
+    main(args.url, args.output, args.ignore, args.path, args.fix, args.index)
